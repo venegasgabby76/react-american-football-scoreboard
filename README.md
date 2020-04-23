ff3881bac654259ea783f5a5e51c1148a48fee21
Gabby Venegas <LJesus@Gabbys-Air-2.attlocal.net>
2020-04-23 14:40:34 -0700

    started working on some stretch, have to get the penalty box to work.

--- README.md
+++ README.md
@@ -58,11 +58,11 @@ const [value, setValue] = useState(); // Give these better names, and decide whe
 
 ### STEP 4 - Add Click Functionality to Increment the Scores
 
-- [ ] Add the `onClick` handler to each function
-- [ ] Determine how much you will need to increment the score for each button
-  - [ ] A touchdown is worth 7 points (assume the following extra point is made)
-  - [ ] A field goal is worth 3 points
-- [ ] Inside the click handlers on each button, use the setter functions for each team to increment the appropriate team's score by the correct amount.
+- [ X ] Add the `onClick` handler to each function
+- [ X ] Determine how much you will need to increment the score for each button
+  - [ X ] A touchdown is worth 7 points (assume the following extra point is made)
+  - [ X ] A field goal is worth 3 points
+- [ X ] Inside the click handlers on each button, use the setter functions for each team to increment the appropriate team's score by the correct amount.
 
 ## Stretch Problems
 
--- src/App.js
+++ src/App.js
@@ -10,10 +10,11 @@ function App() {
   //first we are going ot be declaring the sates here
   // number one will go to the lions. 
   let [lions, lionsCount] = useState(0);
+  let [blackhawlks , blackhawlsCount] = useState(0);
 
   //number two will be going to the tigers.
   let [tigers, tigersCount] = useState(0);
-
+  let [redwings , redwingsCount] = useState(0);
 
 
   return (
@@ -21,7 +22,7 @@ function App() {
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">BLACKHAWKS</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
   <div className="home__score">{lions}</div>
@@ -29,9 +30,9 @@ function App() {
           </div>
 
 
-          <div className="timer">00:03</div>
+          <div className="timer">00:00</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">RED WINGS</h2>
   <div className="away__score">{tigers}</div>
           </div>
         </div>
@@ -43,32 +44,49 @@ function App() {
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button className="homeButtons__touchdown" 
             onClick = {() => {
-              lionsCount(lions +7);
+              lionsCount(lions +1);
                 }}
                   >
-           Home Touchdown
+           Blackhawlks Score
            </button>
           
-          <button className="homeButtons__fieldGoal"
+          <button className="homeButton_pentalty"
             onClick = {() => {
-              lionsCount(lions +3);
+              blackhawlsCount(blackhawlks +1);
                 }}
                   >
-            Home Field Goal</button>
+            Blackhawlks Penalty</button>
+
+            <button className="reset_home"
+          onClick={() => {
+            lionsCount(0);
+          }}
+        >
+          Reset
+        </button>
         </div>
+
         <div className="awayButtons">
           <button className="awayButtons__touchdown"
             onClick = {() => {
-              tigersCount(lions +7);
+              tigersCount(tigers +1);
                 }}
                   >
-            Away Touchdown</button>
-          <button className="awayButtons__fieldGoal"
+            Red Wings Score</button>
+          <button className="awayButton_penalty"
             onClick = {() => {
-              tigersCount(lions +3);
+              redwingsCount(redwings +1);
                 }}
                  >
-                Away Field Goal</button>
+                Red Wings Penalty</button>
+
+                <button className="reset_away"
+          onClick={() => {
+            tigersCount(0);
+          }}
+        >
+          Reset
+        </button>
         </div>
       </section>
     </div>
